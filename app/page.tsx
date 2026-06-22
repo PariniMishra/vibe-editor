9b6b693b904328868fe1fdac05635dd1504ab790
PariniMishra <mishraparini@gmail.com>
2026-06-23 03:07:54 +0530

    authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
 import { Button } from "@/components/ui/button";
+import { User } from "lucide-react";
+import  UserButton  from "@/modules/auth/components/user-button";
 import Image from "next/image";
 
 export default function Home() {
   
   return (
+    <>
    <Button>
     Get Started
    </Button>
+   <UserButton/>
+   </>
   );
 }
